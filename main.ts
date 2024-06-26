6a8214e4a9d3909817db52de053b22c8f3da1b02
CodeWithShary <159939005+CodeWithShary@users.noreply.github.com>
2024-06-27 01:10:37 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+while(condition)
+{    
+let answer = await inquirer.prompt
+(
+  [  
+    {
+        name:'todo',
+        type:'input',
+        message:'What you want to add in your todo list?'
+    },
+    {
+        name:"add",
+        type:"confirm",
+        message:"Do you want to add more?",
+        default:"false"
+    }
+  ]
+);
+todos.push(answer.todo);
+condition = answer.add
+console.log(todos);
+
+}
+
+
+
+
+
+
+
+
+
+
